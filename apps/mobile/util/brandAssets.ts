e03ad5a435b621e7553dab75d7573c4e193d5b62
peter4tech <peterbozz1996@gmail.com>
2026-03-23 12:49:55 +0100

    mobileapp-buld-fix

--- apps/mobile/util/brandAssets.ts
+++ apps/mobile/util/brandAssets.ts
@@ -1,9 +1,18 @@
-import { Image } from 'react-native';
+import { Asset } from 'expo-asset';
 
 export const BRAND_LOGO_ASSET = require('../assets/images/ClaudyGoLogo.webp');
 export const BRAND_HERO_ASSET = require('../assets/images/FB_IMG_1743103252303.jpg');
 export const BRAND_COVER_ASSET = require('../assets/images/CoverArt.webp');
-export const BRAND_LOGO_URI = Image.resolveAssetSource(BRAND_LOGO_ASSET).uri;
-export const BRAND_HERO_URI = Image.resolveAssetSource(BRAND_HERO_ASSET).uri;
-export const BRAND_COVER_URI = Image.resolveAssetSource(BRAND_COVER_ASSET).uri;
-export const DEFAULT_CONTENT_IMAGE_URI = BRAND_COVER_URI;
+
+function resolveBrandAssetUri(moduleId: number): string {
+  try {
+    return Asset.fromModule(moduleId).uri;
+  } catch {
+    return '';
+  }
+}
+
+export const BRAND_LOGO_URI = resolveBrandAssetUri(BRAND_LOGO_ASSET);
+export const BRAND_HERO_URI = resolveBrandAssetUri(BRAND_HERO_ASSET);
+export const BRAND_COVER_URI = resolveBrandAssetUri(BRAND_COVER_ASSET);
+export const DEFAULT_CONTENT_IMAGE_URI = BRAND_COVER_URI || BRAND_HERO_URI || BRAND_LOGO_URI;
